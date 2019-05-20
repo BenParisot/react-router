bb598f790cb59bbc5317fe17ae6d6e6dd26e3b97
Ben Parisot <me@benparisot.com>
2019-05-20 11:28:39 -0700

    user can choose nav color and see color square

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import {
-    BrowserRouter as Route 
-    Route 
+  BrowserRouter as Router, 
+  Route,
+  Switch 
 } from 'react-router-dom';
-import Red from '../components/Colors/'
+import Red from '../components/Colors/Red';
+import Blue from '../components/Colors/Blue';
+import Yellow from '../components/Colors/Yellow';
+import Header from '../components/Header';
+
+export default function App() {
+  return (
+        <>
+            <Router>
+              <Header />
+              <Switch>
+                <Route path="/red" component={Red} />
+                <Route path="/blue" component={Blue} />
+                <Route path="/yellow" component={Yellow} />
+              </Switch>
+            </Router>
+        </>
+  );
+}
---
+++ src/components/Colors/Yellow.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Yellow() {
+  return <div style={{ width: '500px', height: '500px', backgroundColor: 'yellow' }}></div>;
+}
