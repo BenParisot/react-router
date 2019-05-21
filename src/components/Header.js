e551e6581ea6407a32f59bdaeb66ed78e91bf150
Ben Parisot <me@benparisot.com>
2019-05-20 18:07:58 -0700

    write snapshot test for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,13 @@ import {
 
 //this needs to be a class that has state
 export default function Header() {
-    //state should be user color choice
+  //state should be user color choice
   return (
     <header>
       <nav>
-        <Link to="/red">Red</Link>
+        <Link to="/red"></Link>
         <Link to="/blue">Blue</Link>
         <Link to="/yellow">Yellow</Link>
-        //link should go to this.state.color
         <Link to="/:color">Pick A Color</Link>
       </nav>
     </header>
